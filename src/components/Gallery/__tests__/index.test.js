6d770d2e266ad260e49b3c6614e9b2a9660f47e8
cpkopp <cameronpkopp@gmail.com>
2022-12-14 18:03:55 -1000

    gallery set and tested

---
+++ src/components/Gallery/__tests__/index.test.js
@@ -0,0 +1,27 @@
+//DECLARATIONS: react, render+cleanup, jest-dom, Gallery Component --------------
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Gallery from '..';
+
+const portrait = { name: "portraits", description: "portraits of people in my life" };
+
+afterEach(cleanup);
+
+describe('Gallery is rendering', () => {
+
+    it('renders', () => {
+        render(<Gallery currentCategory={portrait} />);
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Gallery currentCategory={portrait} />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('renders h1', () => {
+        const { getByTestId } = render(<Gallery currentCategory={portrait} />);
+        expect(getByTestId('h1tag')).toHaveTextContent('Portraits');
+    });
+
+});
